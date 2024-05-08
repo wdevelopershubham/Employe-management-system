e71f1ddb1b9f409d04658858e51e63e902433307
shubham <shubham763288@gmail.com>
2024-05-08 20:23:09 +0530

    new

---
+++ src/data/index.js
@@ -0,0 +1,69 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Shubham',
+    lastName: 'Kumar',
+    email: 'Shubham@gmail.com',
+    salary: '95000',
+    date: '2024-05-03'
+  },
+  {
+    id: 2,
+    firstName: 'Arnav ',
+    lastName: 'Raj',
+    email: 'arnavraj@gmail.com',
+    salary: '80000',
+    date: '2024-05-03'
+  },
+  {
+    id: 3,
+    firstName: 'Prem',
+    lastName: 'Raj',
+    email: 'prem@gmail.com',
+    salary: '79000',
+    date: '2024-05-03'
+  },
+  {
+    id: 4,
+    firstName: 'Saurabh',
+    lastName: 'Gupta',
+    email: 'Gupta@gmail.com',
+    salary: '56000',
+    date: '2024-05-03'
+  },
+  {
+    id: 5,
+    firstName: 'Saurabh kumar',
+    lastName: 'Ray',
+    email: 'Saurabh@gmail.com',
+    salary: '65000',
+    date: '2024-05-03'
+  },
+  {
+    id: 6,
+    firstName: 'Aman',
+    lastName: 'Akhtar',
+    email: 'aman@gmail.com',
+    salary: '120000',
+    date: '2024-05-03'
+  },
+  {
+    id: 7,
+    firstName: 'Rishav',
+    lastName: 'Sharma',
+    email: 'Rishav@gmail.com',
+    salary: '90000',
+    date: '2024-05-03'
+  },
+  {
+    id: 8,
+    firstName: 'Ayush',
+    lastName: 'Kumar',
+    email: 'Aysh@gmail.com',
+    salary: '60000',
+    date: '2024-05-03'
+  },
+  
+];
+
+export { employeesData };
